e92d227f3d449bbac4374000576055d8d6c43c33
Viswaijth K S <viswajithks.pz@gmail.com>
2025-11-28 02:00:13 +0530

    Fix dirname for vite build

--- web-interface/vite-config.ts
+++ web-interface/vite-config.ts
@@ -1,5 +1,9 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   base: '/llm-tree-to-fs/',
